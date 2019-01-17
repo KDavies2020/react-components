93237fa119785b432c7ae878d0e2298c8a758deb
Kyle Davies <kdavies311@gmail.com>
2019-01-16 21:08:36 -0800

    Refactored into class compnents

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// TODO
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+
+  };
+
+
+render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+      };
+
+ return (
+       <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.props} </li>
+    );
+  };
+};
+
+let GroceryList = (props) => (
+  <div>
+  <h2>My Grocery List</h2>
+    <ul>
+    {props.todos.map(todo =>
+      <GroceryListItem key= {todo} todo= {todo} />
+    )}
+  </ul>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
